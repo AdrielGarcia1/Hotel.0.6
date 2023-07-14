d89d9b8c3d0fb89ea1219d0226ad90c958643d62
Adriel García <garciaadriel65@gmail.com>
2023-07-14 17:03:20 -0300

    'correccion1'

--- Persistence/ServiceExtensions.cs
+++ Persistence/ServiceExtensions.cs
@@ -11,16 +11,10 @@ namespace Persistence
     {
         public static void AddPersistenceInfraestructure(this IServiceCollection services, IConfiguration configuration)
         {
-            //MigrationsAssembly: Configura el ensamblado donde se mantienen las migraciones para este contexto.
-            //El operador typeof obtiene la instancia System.Type para un tipo.
-            //Ej:   int edad;
-            //      Console.WriteLine((typeof(edad));
-            // La salida por pantalla sera: System.Int32.
-            //Assembly: Obtiene la propiedad Assembly en la que está declarado el tipo.
-            //FullName: Obtiene el nombre para mostrar del ensamblado.
+            // Configuración de DbContext
             services.AddDbContext<HotelAppContex>(options => options.UseSqlServer(configuration.GetConnectionString("MsSqlServer"), c => c.MigrationsAssembly(typeof(HotelAppContex).Assembly.FullName)));
 
-            //Inyeccion de dependencia
+            // Inyección de dependencia para repositorios
             #region Repositories
             services.AddTransient(typeof(IRepository<>), typeof(MyRepository<>));
             #endregion
